f9a49ea501eb8455612f43b3e4582d375df571b8
Dorivis <dorival.jrr@gmail.com>
2025-08-15 10:03:15 -0300

    update questions

--- backend/settings.py
+++ backend/settings.py
@@ -131,3 +131,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
